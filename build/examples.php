1f4123bdd463d5987bf99556f4d3a93b9b886a28
Daan Leenders <daan@tracebuzz.com>
2015-06-12 00:38:03 +0200

    Updated .json with newest commits from emoji-data Deleted emoji without image from css and example Finished dir movements Changed vendor examples to 1 comprehencive example

--- build/examples.php
+++ build/examples.php
@@ -1,46 +1,56 @@
 <?php
-$emoji = json_decode(file_get_contents("../emoji.json"));
+$emoji = json_decode(file_get_contents("../src/emoji.json"));
 
 $sheets = array("apple", "emojione", "google", "twitter");
 $sizes = array(16, 20, 32, 64);
+
 /*
- * Generate basic example to demonstrate the css
+ * Generate example to demonstrate the supported emoji by css/images
  */
+echo "Building supported emoji example...<br>";
+$css = fopen("../src/supportedEmoji.html", "w");
+
+fwrite($css, "
+    <!doctype html>
+    <html>
+    <head>
+        <title>View the supported emoji</title>
+        <link rel='stylesheet' href='css/emoji-".array_shift(array_values($sheets)).".css' id='sheet' >
+    </head>
+    <body>
+        <h1>View the supported emoji</h1>
+        <div>
+            Select the requested emoji style.<br>");
 
 foreach($sheets as $sheet) {
-    echo "Building ". $sheet ." example...<br>";
-    $css = fopen("../examples/basic/". $sheet .".html", "w");
-
     fwrite($css, "
-        <!doctype html>
-        <html>
-        <head>
-            <title>Basic example ". $sheet ."</title>
-            <link rel='stylesheet' href='/css/emoji-".$sheet.".css'>
-        </head>
-        <body>
-            <h1>Basic example ". $sheet ."</h1>");
-
-    foreach($sizes as $size) {
-        fwrite($css, "
-            <h2>Size ". $size ."</h2>
-            <div class='emoji-container emoji-".$size."'>");
-
-        foreach($emoji as $item) {
-            fwrite($css, "
-            <i class='emoji e".$item->unicode." '></i>");
-
-            foreach($item->skinVariations as $variation)
-                fwrite($css, "
-            <i class='emoji e".$variation->unicode." '></i>");
-        }
-
-        fwrite($css, "
-            </div>");
-    }
-    fwrite($css, "
-        </body>
-        </html>");
+            <button onclick='document.getElementById(\"sheet\").setAttribute(\"href\", \"css/emoji-". $sheet .".css\");'>". $sheet ."</button>");
 }
+
+fwrite($css, "
+        </div>");
+
+foreach($sizes as $size) {
+    fwrite($css, "
+        <h2>Size ". $size ."</h2>
+        <div class='emoji-container emoji-".$size."'>");
+
+    foreach($emoji as $item) {
+        if($item->imageFile === null)
+            continue;
+
+        fwrite($css, "
+        <i class='emoji e".$item->unicode." '></i>");
+
+        foreach((array) $item->skinVariations as $variation)
+            fwrite($css, "
+        <i class='emoji e".$variation->unicode." '></i>");
+    }
+
+    fwrite($css, "
+        </div>");
+}
+fwrite($css, "
+    </body>
+    </html>");
 ?>
-<link href="../src/css/emoji-apple.css" rel="stylesheet"
--- build/stylesheets.php
+++ build/stylesheets.php
@@ -10,11 +10,11 @@ foreach($sheets as $sheet) {
 
     foreach($sizes as $size) {
         $fileName = "sheet_".$sheet."_".$size.".png";
-        copy("../emoji-data/". $fileName, "../img/".$fileName);
+        copy("../emoji-data/". $fileName, "../src/img/".$fileName);
             
         fwrite($css, "
 .emoji-". $size ." .emoji{
-    background-image: url('img/". $fileName ."');
+    background-image: url('../img/". $fileName ."');
     width: ". $size ."px;
     height: ". $size ."px;
     display: -moz-inline-stack;
@@ -24,8 +24,11 @@ foreach($sheets as $sheet) {
 }\n");
 
         foreach($emoji as $item) {
+            if($item->imageFile === null)
+                continue;
+            
             fwrite($css, ".emoji-". $size ." .emoji.e". $item->unicode . "{background-position:-" . ($item->sheetX * $size) . "px -" . ($item->sheetY * $size) . "px;} ");
-            foreach($item->skinVariations as $variation)
+            foreach((array) $item->skinVariations as $variation)
                 fwrite($css, ".emoji-". $size ." .emoji.e". $variation->unicode . "{background-position:-" . ($variation->sheetX * $size) . "px -" . ($variation->sheetY * $size) . "px;} ");
         }
         fwrite($css, "\n\n");
